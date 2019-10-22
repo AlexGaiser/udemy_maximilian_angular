2b36b171e152f39237592a3a742a2a3e95a23eb1
AlexGaiser <alexcgaiser@gmail.com>
2019-10-21 21:51:33 -0400

    filter pipe

--- src/app/filter.pipe.ts
+++ src/app/filter.pipe.ts
@@ -5,8 +5,8 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class FilterPipe implements PipeTransform {
 
-  transform(value: any, ...args: any[]): any {
-    return null;
+  transform(value: any, filterString: string): any {
+    return ;
   }
 
 }
